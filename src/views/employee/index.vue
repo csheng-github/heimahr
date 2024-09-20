6e6ec2e8101e8d61a0e250a72fa7f89a8eac48a9
陈升 <cs@baigebao.com>
2024-09-20 16:47:47 +0800

    update

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -1,12 +1,169 @@
 <template>
   <div class="container">
     <div class="app-container">
-      员工
+      <div class="left">
+        <el-input
+          v-model="queryParams.keyword"
+          style="margin-bottom:10px"
+          type="text"
+          prefix-icon="el-icon-search"
+          size="small"
+          placeholder="输入员工姓名全员搜索"
+          @input="changeValue"
+        />
+        <el-tree
+          ref="deptTree"
+          node-key="id"
+          :data="depts"
+          :props="defaultProps"
+          default-expand-all
+          :expand-on-click-node="false"
+          highlight-current
+          @current-change="selectNode"
+        />
+      </div>
+      <div class="right">
+        <el-row class="operate-tools" type="flex" justify="end">
+          <el-button size="mini" type="primary">添加员工</el-button>
+          <el-button size="mini">excel导入</el-button>
+          <el-button size="mini">excel导出</el-button>
+        </el-row>
+        <el-table :data="list">
+          <el-table-column prop="staffPhoto" align="center" label="头像">
+            <template v-slot="{ row }">
+              <el-avatar v-if="row.staffPhoto" :src="row.staffPhoto" :size="30" />
+              <span v-else class="username">{{ row.username?.charAt(0) }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="username" label="姓名" />
+          <el-table-column prop="mobile" label="手机号" sortable />
+          <el-table-column prop="workNumber" label="工号" sortable />
+          <el-table-column prop="formOfEmployment" label="聘用形式">
+            <template v-slot="{ row }">
+              <span v-if="row.formOfEmployment === 1">正式</span>
+              <span v-else-if="row.formOfEmployment === 2">非正式</span>
+              <span v-else>无</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="departmentName" label="部门" />
+          <el-table-column prop="timeOfEntry" label="入职时间" sortable />
+          <el-table-column label="操作" width="280px">
+            <template>
+              <el-button size="mini" type="text">查看</el-button>
+              <el-button size="mini" type="text">角色</el-button>
+              <el-button size="mini" type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-row style="height: 60px" align="middle" type="flex" justify="end">
+          <el-pagination
+            layout="total,prev, pager, next"
+            :total="total"
+            :current-page="queryParams.page"
+            :page-size="queryParams.pagesize"
+            @current-change="changePage"
+          />
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
+import { transListToTreeData } from '@/utils'
+import { getDepartment } from '@/api/department'
+import { getEmployeeList } from '@/api/employee'
+
 export default {
-  name: 'Employee'
+  name: 'Employee',
+  data() {
+    return {
+      depts: [],
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      },
+
+      list: [],
+      queryParams: {
+        departmentId: null,
+        page: 1,
+        pagesize: 10,
+        keyword: ''
+      },
+      total: 0,
+
+      timer: null
+    }
+  },
+  created() {
+    this.getDepartment()
+  },
+  methods: {
+    async getDepartment() {
+      this.depts = transListToTreeData(await getDepartment(), 0)
+      this.queryParams.departmentId = this.depts[0].id
+      this.$nextTick(() => {
+        this.$refs.deptTree.setCurrentKey(this.queryParams.departmentId)
+      })
+      this.getEmployeeList()
+    },
+
+    async getEmployeeList() {
+      const { rows, total } = await getEmployeeList(this.queryParams)
+      this.list = rows
+      this.total = total
+    },
+
+    selectNode(node) {
+      this.queryParams.departmentId = node.id
+      this.queryParams.page = 1
+      this.getEmployeeList()
+    },
+
+    changePage(newPage) {
+      this.queryParams.page = newPage
+      this.getEmployeeList()
+    },
+
+    changeValue() {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.queryParams.page = 1
+        this.getEmployeeList()
+      }, 300)
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.app-container {
+  display: flex;
+  background: #fff;
+  .left {
+    width: 280px;
+    padding: 20px;
+    border-right: 1px solid #eaeef4;
+  }
+  .right {
+    flex: 1;
+    padding: 20px;
+    .operate-tools {
+      margin: 10px ;
+    }
+    .username {
+      height: 30px;
+      width: 30px;
+      line-height: 30px;
+      text-align: center;
+      border-radius: 50%;
+      color: #fff;
+      background: #04C9BE;
+      font-size: 12px;
+      display:inline-block;
+    }
+  }
+}
+
+</style>
