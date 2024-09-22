27d2ce6f564fe9ad3707961462be877729452250
陈升 <cs@baigebao.com>
2024-09-23 02:14:15 +0800

    update

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,15 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-
-  /* 自定义全局变量 */
+  /* 登录标识 */
   token: state => state.user.token,
+  /* 用户信息 */
   userId: state => state.user.userInfo.userId,
   avatar: state => state.user.userInfo.staffPhoto,
   name: state => state.user.userInfo.username,
+  company: state => state.user.userInfo.company,
+  departmentName: state => state.user.userInfo.departmentName,
+  /* 路由 */
   routes: state => state.user.routes
 }
 export default getters
